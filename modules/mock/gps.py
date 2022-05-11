2a91a89943d0fb455c86fe964ba558c2f4e07023
Christophe-A <christo-chibougamo@hotmail.com>
2022-05-11 11:13:11 -0400

    First draft real gps, testing for working serial connection.

---
+++ modules/mock/gps.py
@@ -0,0 +1,21 @@
+from socket import timeout
+import serial 
+
+from modules.mqtt_utils import MqttTopics
+from mqtt_modules import MqttPubModule
+
+class ZED_F9P_Hat_GPS(MqttPubModule):
+    def __init__(self, mqtt_broker: str = "localhost", mqtt_broker_port=1883, serial_port="/dev/ttyS0", baud="38400") -> None:
+        super().__init__([MqttTopics.POSITION, MqttTopics.SPEED], mqtt_broker, mqtt_broker_port)
+
+        self.serial_con = serial.Serial(serial_port, baud, timeout=5)
+
+    def run(self): # This is an infinite loop pooling the gps
+        while True:
+            line = self.serial_con.readline()
+            print(f"-- {line}")
+
+class GPS(ZED_F9P_Hat_GPS): pass
+
+gps = GPS()
+gps.run()
