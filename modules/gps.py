377a1e5536af4c475962ffc1867229316aa6f5e2
Christophe-A <christo-chibougamo@hotmail.com>
2022-05-11 11:17:12 -0400

    Fixed wrong location for gps module

--- modules/gps.py
+++ modules/gps.py
@@ -1,8 +1,8 @@
 from socket import timeout
 import serial 
 
-from modules.mqtt_utils import MqttTopics
-from mqtt_modules import MqttPubModule
+from .mqtt_utils import MqttTopics
+from .mqtt_modules import MqttPubModule
 
 class ZED_F9P_Hat_GPS(MqttPubModule):
     def __init__(self, mqtt_broker: str = "localhost", mqtt_broker_port=1883, serial_port="/dev/ttyS0", baud="38400") -> None:
