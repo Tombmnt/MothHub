6acad3c431e36333a6dd5af5166a89047b9a42e5
Christophe-A <christo-chibougamo@hotmail.com>
2022-05-12 17:41:24 -0400

    Actually sending gps mqtt packets

---
+++ modules/data_models/mqtt_packets.py
@@ -0,0 +1,85 @@
+
+from dataclasses import dataclass
+import json
+from typing import Any
+
+# Default init values "defines"
+DEFAULT_TIMESTAMP = 0
+DEFAULT_SENDER_TYPE = "null"
+
+DEFAULT_LONGITUDE = 0.0 
+DEFAULT_LON_DIR = "W" 
+DEFAULT_LATITUDE = 0.0, 
+DEFAULT_LAT_DIR = "N"
+
+DEFAULT_SPEED = 0.0
+DEFAULT_SPEED_UNIT = "N"
+DEFAULT_DIRECTION = "0.0"
+DEFAULT_DIRECTION_UNIT = "T"
+
+@dataclass
+class SenderTypes:
+    gps = "gps",
+    imu = "imu"
+    other = "unknown"
+
+class MQTTPacket:
+    def __init__(self, 
+        timestamp: int = DEFAULT_TIMESTAMP, 
+        sender_type: str = DEFAULT_SENDER_TYPE
+    ):
+        self.time = timestamp
+        self.type = sender_type
+
+    def __str__(self) -> str:
+        return json.dumps(self.__dict__)
+    
+    def add_data(self, name: str, value: Any):
+        setattr(self, name, value)
+    
+    def add_data_mul_dict(self, data_dict: dict):
+        for n, v in data_dict.items():
+            setattr(self, n, v)
+    
+    def reset_to_default(self):
+        for attr in self.__dict__.keys():
+            delattr(self, attr)
+        self.__init__()
+
+class MQTTPositionPkt(MQTTPacket):
+    def __init__(self, 
+        timestamp: int = DEFAULT_TIMESTAMP, 
+        sender_type: str = DEFAULT_SENDER_TYPE, 
+        longitude: float = DEFAULT_LONGITUDE, 
+        lon_dir: str = DEFAULT_LON_DIR, 
+        latitude: float = DEFAULT_LATITUDE, 
+        lat_dir: str = DEFAULT_LAT_DIR
+    ):
+        super().__init__(timestamp, sender_type)
+        self.lon = longitude
+        self.lonEW = lon_dir
+        self.lat = latitude
+        self.latNS = lat_dir
+
+class MQTTSpeedPkt(MQTTPacket):
+    def __init__(self, 
+        timestamp: int = DEFAULT_TIMESTAMP, 
+        sender_type: str = DEFAULT_SENDER_TYPE,
+        speed: float = DEFAULT_SPEED,
+        speed_unit: str = DEFAULT_SPEED_UNIT,
+        direction: float = DEFAULT_DIRECTION,
+        direction_unit: str = DEFAULT_DIRECTION_UNIT
+    ):
+        super().__init__(timestamp, sender_type)
+        self.speed = speed,
+        self.spd_unit = speed_unit,
+        self.dir = direction,
+        self.dir_unit = direction_unit
+
+class MQTTOrientationPkt(MQTTPacket):
+    def __init__(self, 
+        timestamp: int = DEFAULT_TIMESTAMP,
+        sender_type: str  = DEFAULT_SENDER_TYPE
+    ):
+        super().__init__(timestamp, sender_type)
+        # TODO: Add pertinent attributes
--- modules/gps.py
+++ modules/gps.py
@@ -1,8 +1,28 @@
+import datetime
+import json
+from turtle import pos
+from xmlrpc.client import DateTime
 import serial 
-from pynmeagps import NMEAReader
+from pynmeagps import NMEAReader, NMEAMessage
+
+from modules.data_models.mqtt_packets import MQTTPositionPkt, MQTTSpeedPkt, SenderTypes
 
 from .mqtt_utils import MqttTopics
 from .mqtt_modules import MqttPubModule
+
+# Available message types: 
+# ['$GNRMC', '$GNVTG', '$GNGGA', '$GNGSA', '$GPGSV', '$GLGSV', '$GAGSV', '$GBGSV', '$GNGLL']
+
+# Used:
+# RMC: Position, time
+# VTG: Speed over ground
+# GGA: Position, quality
+NMEA_MESSAGE_GSA = "GSA"
+NMEA_MESSAGE_RMC = "RMC"
+NMEA_MESSAGE_VTG = "VTG"
+NMEA_MESSAGE_GGA = "GGA"
+NMEA_MESSAGE_GSV = "GSV"
+NMEA_MESSAGE_GLL = "GLL"
 
 class ZED_F9P_Hat_GPS(MqttPubModule):
     def __init__(self, mqtt_broker: str = "localhost", mqtt_broker_port=1883, serial_port="/dev/ttyS0", baud="38400") -> None:
@@ -13,8 +33,51 @@ class ZED_F9P_Hat_GPS(MqttPubModule):
 
     def run(self): # This is an infinite loop pooling the gps
         while True:
-            raw_data, parsed_data = self.nmea_stream.read()
-            print(f"{parsed_data}")
+            parsed_msg: NMEAMessage = None
+            _, parsed_msg = self.nmea_stream.read()
+            # TODO: actuall logging 
+            print(f"{parsed_msg}")
+
+            pos_data = MQTTPositionPkt(sender_type=SenderTypes.gps)
+            dt: datetime.datetime = 0.0
+                
+            if(parsed_msg.msgID == NMEA_MESSAGE_RMC):
+                # RMC message is the first relevant message we get per data burst, we can use it to send the timestamp for the whole burst.
+                dt = datetime.datetime.combine(parsed_msg.date, parsed_msg.time).astimezone(datetime.timezone.utc)
+                pos_data.time = int(dt.timestamp()) # time as a unix timestamp
+                pos_data.lon = parsed_msg.lon
+                pos_data.lonEW = parsed_msg.EW
+                pos_data.lat = parsed_msg.lat
+                pos_data.latNS = parsed_msg.NS
+                pos_data.add_data("nav_status", parsed_msg.navStatus)
+
+            elif(parsed_msg.msgID == NMEA_MESSAGE_GGA):
+                pos_data.add_data("quality", parsed_msg.quality)
+                pos_data.add_data("sat_nbr", parsed_msg.numSV)
+                pos_data.add_data("h_precision", parsed_msg.HDOP) #in meters
+                pos_data.add_data("altitude", parsed_msg.alt)
+                pos_data.add_data("alt_unit", parsed_msg.altUnit)
+                pos_data.add_data("geoid_sep", parsed_msg.sep)
+                pos_data.add_data("geoid_sep_unit", parsed_msg.sepUnit)
+
+                # The GGA message is the last (relevant) message we get for each data burst
+                self.publish(MqttTopics.POSITION, str(pos_data))
+                pos_data.reset_to_default()
+                pos_data.type = SenderTypes.gps
+                dt = 0.0
+
+            elif(parsed_msg.msgID == NMEA_MESSAGE_VTG):
+                spd_data = MQTTSpeedPkt(
+                    timestamp = int(dt.timestamp()),
+                    sender_type = SenderTypes.gps,
+                    speed = parsed_msg.sogn, # Speed by default in knots
+                    speed_unit = parsed_msg.sognUnit,
+                    direction = parsed_msg.cogt, # Course over groub by default in degree true
+                    direction_unit = parsed_msg.cogtUnit
+                )
+                spd_data.add_data_mul_dict({"speed_kmh": parsed_msg.sogk, "dir_mag": parsed_msg.cogm})
+
+                self.publish(MqttTopics.POSITION, str(spd_data))
 
 class GPS(ZED_F9P_Hat_GPS): pass
 
