58ee73013e32c0cb680139e39dc14f97dc92932a
Christophe-A <christo-chibougamo@hotmail.com>
2022-05-29 18:35:25 -0400

    Half implemented shore com

---
+++ modules/shore_com.py
@@ -0,0 +1,85 @@
+import json
+import logging as log
+from threading import Lock
+
+import serial
+import paho.mqtt.client as mqtt
+
+from modules.mqtt_utils import MqttTopics
+from .mqtt_modules import MqttSubModule
+
+class RF_REGION_MHZ:
+    NORTH_AMERICA = 903.3
+    EUROPE = 869.3
+
+class AT_COMMANDS:
+    AT = ""
+    ID = "ID"
+    MODE = "MODE"
+    PTP = "TEST"
+
+class MODES:
+    TRANSMIT = "tx"
+    RECIEVE = "rx"
+
+class Serial_AT_Module:
+    def __init__(self, serial_port: str, baud: int, region_MHz: float, start_mode: str) -> None:
+        self.command_in_use = Lock()
+
+        self.serial_connection = serial.Serial(serial_port, baud, timeout=30)
+
+        self._sendCommand(AT_COMMANDS.MODE, AT_COMMANDS.PTP)
+        self._sendCommand(AT_COMMANDS.PTP, "RFCFG," + region_MHz + ",SF7,125,12,15,14,ON,OFF,OFF")
+        
+        self.isReciever = (start_mode is MODES.RECIEVE)
+        if self.isReciever:
+            # TODO: handle being a reciever
+            #self.callback = recieveCallback
+            #self._sendCommand(AT_COMMANDS.PTP, "RXLRPKT")
+            #self._loopRecieve()
+            pass
+
+    def _sendCommand(self, command, parameterString):
+        self.command_in_use.acquire()
+        
+        msg = "AT"
+        if(command):
+            msg += "+" + command
+            
+            if(parameterString):
+                msg += "=" + parameterString
+        
+        msg += "\n\r"
+
+        bmsg = bytes(msg, 'utf-8')
+        self.serial_connection.write(bmsg)
+
+        # TODO: a proper way to check that transaction is done
+        line = self.serial_connection.readline()
+        line = self.serial_connection.readline()
+
+        self.command_in_use.release()
+
+    def sendString(self, string) -> bool:
+        if(not self.isReciever):
+            self._sendCommand(AT_COMMANDS.PTP, "TXLRSTR,\""+string+"\"")
+            return True
+        else:
+            return False
+
+        
+
+class LoRa_E5(MqttSubModule):
+    def __init__(self, region_MHz: float, start_mode: str, serial_port: str, baud: int = 9600, mqtt_broker: str = "localhost", mqtt_broker_port: int = 1883) -> None:
+        super().__init__(MqttTopics.ALL, mqtt_broker, mqtt_broker_port)
+
+        self.at_modem = Serial_AT_Module(serial_port, baud, region_MHz, start_mode)
+
+        # Exceptionaly overwriting the on_message method of the mqtt client because we want it called on ANY topic.
+        self.mqtt_clients[MqttTopics.ALL].client.on_message = self.on_message
+
+    def on_message(self, client: mqtt.Client, userdata, msg: mqtt.MQTTMessage):
+        log.debug(f"Recieved: [{msg.topic}]:{msg.payload}\n")
+        string_msg = json.dumps({msg.topic: msg.payload})
+        self.at_modem.sendString(string_msg)
+
