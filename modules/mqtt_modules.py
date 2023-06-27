fcbd3b09266552a0d3bd757d7cdd236ab2012e3f
Christophe-A <christo-chibougamo@hotmail.com>
2023-06-27 06:22:18 -0400

    option to not autostart a mqtt module

--- modules/mqtt_modules.py
+++ modules/mqtt_modules.py
@@ -3,11 +3,11 @@ import paho.mqtt.client as mqtt
 from modules.mqtt_utils import MqttPubClient, MqttSubClient
 
 class MqttSubModule:
-    def __init__(self, mqtt_topics: "list[str]", mqtt_broker: str = "localhost", mqtt_broker_port: int = 1883) -> None:
+    def __init__(self, mqtt_topics: "list[str]", mqtt_broker: str = "localhost", mqtt_broker_port: int = 1883, start=True) -> None:
         self.mqtt_clients: dict[str, MqttSubClient] = {}
         
         for topic in mqtt_topics:
-            self.mqtt_clients[topic] = MqttSubClient(topic, mqtt_broker, mqtt_broker_port)
+            self.mqtt_clients[topic] = MqttSubClient(topic, mqtt_broker, mqtt_broker_port, start)
 
     def register_client_callback(self, topic: str, callback: Callable[[mqtt.Client, Any, mqtt.MQTTMessage], Any]):
         self.mqtt_clients[topic].message_callback_add(topic, callback)
